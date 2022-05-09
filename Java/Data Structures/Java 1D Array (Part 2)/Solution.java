2bd8b7a9be37965b928aebcaff7a95bd24c215df
pshiva <pshiva@yapstone.com>
2022-05-09 18:10:57 +0100

    add solutions to exception and Data structures

---
+++ Java/Data Structures/Java 1D Array (Part 2)/Solution.java
@@ -0,0 +1,26 @@
+// github.com/punithkumar-bs
+
+public static boolean canWin(int leap, int[] game) {
+	if (game == null) {
+		return false;
+	}
+    return isSolvable(leap, game, 0);
+}
+
+private static boolean isSolvable(int leap, int[] game, int i) {
+    // Base Cases
+    if (i >= game.length) {
+        return true;
+    } else if (i < 0 || game[i] == 1) {
+        return false;
+    }
+    
+    game[i] = 1; // marks as visited
+
+    // Recursive Cases (Tries +m first to try to finish game quickly)
+    return isSolvable(leap, game, i + leap)
+        || isSolvable(leap, game, i + 1)
+        || isSolvable(leap, game, i - 1);
+}
+
+// Discuss on HackerRank: https://www.hackerrank.com/challenges/java-1d-array/forum/comments/253767
