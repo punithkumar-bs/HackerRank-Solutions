626b8906fdd90376d13747d52da0bd8c99b13e20
pshiva <pshiva@yapstone.com>
2022-05-10 09:22:26 +0100

    add solutions to Big Number

---
+++ Java/BigNumber/Java BigDecimal/Solution.java
@@ -0,0 +1,37 @@
+// github.com/punithkumar-bs
+
+import java.math.BigDecimal;
+import java.util.*;
+
+class Solution {
+    public static void main(String []args) {
+        //Input
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String [] s = new String[n+2];
+        for (int i = 0; i < n; i++) {
+            s[i] = sc.next();
+        }
+        sc.close();
+        
+        // We want to sort in descending order while preserving the contents 
+        // of each String. A comparator can achieve this for us. We convert  
+        // to BigDecimal inside our comparator so that the change is not 
+        // permanent and our String's form for each number is preserved.
+        Comparator<String> customComparator = new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                BigDecimal a = new BigDecimal(s1);
+                BigDecimal b = new BigDecimal(s2);
+                return b.compareTo(a); // descending order
+            }
+        };
+
+        Arrays.sort(s, 0, n, customComparator);
+        
+        //Output
+        for (int i = 0; i < n; i++) {
+            System.out.println(s[i]);
+        }
+    }
+}
