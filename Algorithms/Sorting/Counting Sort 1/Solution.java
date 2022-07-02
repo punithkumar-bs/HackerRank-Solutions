65f82e5382b00d79aa35e89d5ed0b2a93bca3a99
pshiva <pshiva@yapstone.com>
2022-07-02 13:10:37 +0100

    Add sorting solutions

---
+++ Algorithms/Sorting/Counting Sort 1/Solution.java
@@ -0,0 +1,22 @@
+// github.com/punithkumar-bs
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        final int maxValue = 100;
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int[] array = new int[maxValue];
+        
+        for (int i = 0; i < n; i++) {
+            int num = scan.nextInt();
+            array[num]++;
+        }
+        scan.close();
+        
+        for (int i = 0; i < n; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
+}
---
+++ Algorithms/Sorting/Minimum Absolute Difference in an Array/Solution.java
@@ -0,0 +1,33 @@
+// github.com/punithkumar-bs
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+// Algorithm: Sort the array and compare adjacent pairs
+
+// Greedy algorithm? No
+
+// A greedy algorithm makes "the locally optimal choice at each 
+// stage with the hope of finding a global optimum" - Wikipedia
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int [] array = new int[n];
+        for (int i = 0; i < n; i++) {
+            array[i] = scan.nextInt();
+        }
+        scan.close();
+        System.out.println(minAbsoluteDifference(array));
+    }
+    
+    private static int minAbsoluteDifference(int [] array) {
+        Arrays.sort(array);
+        int min = Integer.MAX_VALUE;
+        for (int i = 1; i < array.length; i++) {
+            min = Math.min(min, Math.abs(array[i] - array[i-1]));
+        }
+        return min;
+    }
+}
