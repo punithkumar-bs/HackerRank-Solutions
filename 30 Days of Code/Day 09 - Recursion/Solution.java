fcb2e3034f73ce4995caebe37e155a1dfb354e95
pshiva <pshiva@yapstone.com>
2022-07-24 23:57:01 +0100

    Add Day 9 Solution

---
+++ 30 Days of Code/Day 09 - Recursion/Solution.java
@@ -0,0 +1,23 @@
+// github.com/punithkumar-bs
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        scan.close();
+        System.out.println(factorial(n));
+    }
+    
+    private static Long factorial (int n) {
+        if (n < 0) {
+            return null;
+        }
+        long result = 1;
+        while (n > 0) {
+            result *= n--;
+        }
+        return result;
+    }
+}
